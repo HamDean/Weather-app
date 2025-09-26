d61da0a177c847b0136f618f4f3896677033b140
Nurudeen Hamzah <hamdeen247@gmail.com>
2025-09-26 08:38:08 +0000

    Extract weather to code into constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -47,23 +47,6 @@ export const unitGroups = [
   },
 ];
 
-export const dummyDetails = [
-  { title: "Feels Like", value: "72°" },
-  { title: "Feels Like", value: "72°" },
-  { title: "Feels Like", value: "72°" },
-  { title: "Feels Like", value: "72°" },
-];
-
-export const dummyForecast = [
-  { day: "Tue", min: 25, max: 55 },
-  { day: "Wed", min: 15, max: 45 },
-  { day: "Thu", min: 75, max: 85 },
-  { day: "Fri", min: 75, max: 85 },
-  { day: "Sat", min: 75, max: 85 },
-  { day: "Sun", min: 75, max: 85 },
-  { day: "Mon", min: 75, max: 85 },
-];
-
 export const days = [
   "Tuesday",
   "Wednessday",
@@ -73,3 +56,60 @@ export const days = [
   "Sunday",
   "Monday",
 ];
+
+import IconStorm from "../assets/images/icon-storm.webp";
+import IconSunny from "../assets/images/icon-sunny.webp";
+import IconRain from "../assets/images/icon-rain.webp";
+import IconPartlyCloudy from "../assets/images/icon-partly-cloudy.webp";
+import IconOverCast from "../assets/images/icon-overcast.webp";
+import IconFog from "../assets/images/icon-fog.webp";
+import IconDrizzle from "../assets/images/icon-drizzle.webp";
+import IconSnow from "../assets/images/icon-snow.webp";
+
+export const weatherCodeToIcon = {
+  // Clear sky
+  0: IconSunny,
+
+  // Cloudiness
+  1: IconSunny, // Mainly clear
+  2: IconPartlyCloudy, // Partly cloudy
+  3: IconOverCast, // Overcast
+
+  // Fog
+  45: IconFog, // Fog
+  48: IconFog, // Depositing rime fog
+
+  // Drizzle
+  51: IconDrizzle, // Light drizzle
+  53: IconDrizzle, // Moderate drizzle
+  55: IconDrizzle, // Dense drizzle
+  56: IconDrizzle, // Freezing drizzle, light
+  57: IconDrizzle, // Freezing drizzle, dense
+
+  // Rain
+  61: IconRain, // Slight rain
+  63: IconRain, // Moderate rain
+  65: IconRain, // Heavy rain
+  66: IconRain, // Freezing rain, light
+  67: IconRain, // Freezing rain, heavy
+
+  // Snow
+  71: IconSnow, // Slight snow fall
+  73: IconSnow, // Moderate snow fall
+  75: IconSnow, // Heavy snow fall
+  77: IconSnow, // Snow grains
+
+  // Rain showers
+  80: IconRain, // Slight rain showers
+  81: IconRain, // Moderate rain showers
+  82: IconRain, // Violent rain showers
+
+  // Snow showers
+  85: IconSnow, // Slight snow showers
+  86: IconSnow, // Heavy snow showers
+
+  // Thunderstorms
+  95: IconStorm, // Thunderstorm, slight or moderate
+  96: IconStorm, // Thunderstorm with slight hail
+  99: IconStorm, // Thunderstorm with heavy hail
+};
